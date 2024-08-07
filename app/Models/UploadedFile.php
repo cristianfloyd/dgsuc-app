c0e81d64958bf80166bd2e04a41f491f0312fff0
cristianfloyd <cristianfloyd@gmail.com>
2024-08-07 20:22:12 -0300

    Reapply "Merge branch 'main' of https://github.com/cristianfloyd/informes-app"
    This reverts commit eb60d3ac40715bcd5cece945da89b1c0a66c5404.

--- app/Models/UploadedFile.php
+++ app/Models/UploadedFile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use App\Contracts\FileUploadRepositoryInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UploadedFile extends Model
@@ -19,6 +20,25 @@ class UploadedFile extends Model
         'user_id',
         'user_name',
     ];
+
+
+    /**
+     * Create a new uploaded file record in the database.
+     *
+     * This method creates a new instance of UploadedFile with the given data
+     * and saves it to the database.
+     *
+     * @param array $data An associative array containing the attributes for the new UploadedFile
+     * @return \App\Models\UploadedFile The newly created UploadedFile instance
+     */
+    public static function create(array $data)
+    {
+        $uploadedFile = new static();
+        $uploadedFile->fill($data);
+        $uploadedFile->save();
+
+        return $uploadedFile;
+    }
 
 
 	/**
