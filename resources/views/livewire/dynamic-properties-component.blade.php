61eb7209fbabbab5aad3ffdaf0dce2a4383c6f2c
cristianfloyd <cristianfloyd@gmail.com>
2024-10-22 16:46:32 -0300

    update widget dinamico de propiedades

--- resources/views/livewire/dynamic-properties-component.blade.php
+++ resources/views/livewire/dynamic-properties-component.blade.php
@@ -3,17 +3,25 @@
         <div>
             <h6>Propiedades del Recurso</h6>
         </div>
-            <ul>
-                @foreach($properties as $key => $value)
+        <ul>
+            @foreach ($properties as $key => $values)
+                @if ($key === 'periodoFiscal' && is_array($values))
                     <li>
-                        <strong>{{ $key }}:</strong>
-                        @if(is_array($value))
-                            {{ json_encode($value) }}
-                        @else
-                            {{ $value }}
-                        @endif
+                        @foreach ($values as $key2 => $value)
+                            <strong>{{ __("$key2") }}: </strong> {{ $value }}
+                        @endforeach
                     </li>
-                @endforeach
-            </ul>
+                    @continue
+                @endif
+                <li>
+                    <strong>{{ $key }}:</strong>
+                    @if (is_array($values) && $key != 'periodoFiscal')
+                        {{ json_encode($values) }}
+                    @else
+                        {{ $values }}
+                    @endif
+                </li>
+            @endforeach
+        </ul>
     </x-filament::section>
 </div>
