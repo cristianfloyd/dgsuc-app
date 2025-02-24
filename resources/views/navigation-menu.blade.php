a9327b74978cdfec513ef3fa9d6b3c7a12c085a7
cristianfloyd <cristianfloyd@gmail.com>
2025-02-23 21:00:01 -0300

    login logout fix

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -206,13 +206,34 @@
                             <div class="border-t border-gray-200 dark:border-gray-600"></div>
 
                             <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
+                            <form method="POST" action="{{ route('logout') }}" x-data="{ loading: false }">
                                 @csrf
-
-                                <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
+                                <x-dropdown-link
+                                    type="button"
+                                    @click.prevent="
+                                        loading = true;
+                                        $el.classList.add('animate-pulse');
+                                        setTimeout(() => {
+                                            $root.submit();
+                                        }, 500)
+                                    "
+                                    x-bind:class="{ 'opacity-50 cursor-wait': loading }"
+                                >
+                                    <div class="flex items-center">
+                                        <span x-show="!loading">{{ __('Log Out') }}</span>
+                                        <span x-show="loading" class="flex items-center">
+                                            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                            </svg>
+                                            Cerrando sesión...
+                                        </span>
+                                    </div>
                                 </x-dropdown-link>
                             </form>
+
+
+
                         </x-slot>
                     </x-dropdown>
                 </div>
