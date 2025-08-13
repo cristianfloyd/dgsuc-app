3df3d0feda673029f4802ee4454f04d8dabea118
Cristian Arenas <cristianfloyd@gmail.com>
2025-08-13 20:10:42 -0300

    Correccion de nombres psr

---
+++ app/Filament/Reportes/Resources/RepGerencialFinalResource/Pages/CreateRepGerencialFinal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\Filament\Reportes\Resources\RepGerencialFinalResource\Pages;
+
+use App\Filament\Reportes\Resources\RepGerencialFinalResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRepGerencialFinal extends CreateRecord
+{
+    protected static string $resource = RepGerencialFinalResource::class;
+}
