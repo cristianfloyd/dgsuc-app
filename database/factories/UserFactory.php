91d419c471ebb3a7ab45bc265d97bb0b5c115e99
cristianfloyd <carenas@uba.ar>
2026-03-09 22:16:14 -0300

    refactor: Improve code formatting in UserFactory
    - Adjusted formatting of closure definitions for consistency.
    - Enhanced readability by removing unnecessary spaces in state functions.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -46,7 +46,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
@@ -62,7 +62,7 @@ class UserFactory extends Factory
 
         return $this->has(
             Team::factory()
-                ->state(fn (array $attributes, User $user) => [
+                ->state(fn(array $attributes, User $user) => [
                     'name' => $user->name . '\'s Team',
                     'user_id' => $user->id,
                     'personal_team' => true,
